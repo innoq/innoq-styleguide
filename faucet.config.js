dc3873dae4fe32fcc39f7b92e2f2ad823e652a00
Roman Stranghöner <roman.stranghoener@innoq.com>
2017-10-04 11:55:25 +0200

    externalize usage of normalization and update dependencies

--- faucet.config.js
+++ faucet.config.js
@@ -20,10 +20,16 @@ const config = {
       baseURI: '/'
     },
     assets: [],
-    bundles: [{
-      entryPoint: './lib/styles/index.scss',
-      target: './public/css/bundle.css'
-    }]
+    bundles: [
+      {
+        entryPoint: './lib/styles/index.scss',
+        target: './public/css/bundle.css'
+      },
+      {
+        entryPoint: './node_modules/normalize.css/normalize.css',
+        target: './public/css/normalize.css'
+      }
+    ]
   }
 }
 
