442658850372ed9ff292d29030542f477626aaef
Robert Glaser <mail@robert-glaser.de>
2017-10-30 15:24:02 +0100

    Use Cloudfront based web fonts

--- components/_preview.jsx
+++ components/_preview.jsx
@@ -7,7 +7,9 @@ export default function PreviewLayout(params, ...children) {
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" type="text/css" href="https://innoq.com/fonts/webfonts.css" />
+          <link rel="stylesheet"
+            type="text/css"
+            href="https://d1feosfg1qz8rt.cloudfront.net/assets/webfonts-9fafb7a845e0cef89527ed771552ec4708731a37c466c9ca82afa1ec57e21e00.css" />
           <link rel="stylesheet" type="text/css" href="/css/normalize.css" />
           <link media="all" rel="stylesheet" href={stylesheetPath} />
           <title>Preview Layout</title>
--- docs/01-index.md
+++ docs/01-index.md
@@ -2,4 +2,8 @@
 title: innoQ Styleguide
 ---
 
-This is your index page. You can edit its contents at `docs/01-index.md`.
+## Web fonts
+
+    <link rel="stylesheet"
+          type="text/css"
+          href="https://d1feosfg1qz8rt.cloudfront.net/assets/webfonts-9fafb7a845e0cef89527ed771552ec4708731a37c466c9ca82afa1ec57e21e00.css" />
