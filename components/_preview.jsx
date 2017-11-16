3556e24eea1d4d13523d4c3d83f13329a654885c
Robert Glaser <mail@robert-glaser.de>
2017-11-16 10:32:29 +0100

    Update complate-fractal settings, lint .jsx

--- components/_preview.jsx
+++ components/_preview.jsx
@@ -1,19 +1,19 @@
-import { createElement } from "complate-stream";
+import { createElement } from 'complate-stream'
 
-export default function PreviewLayout(params, ...children) {
-    const stylesheetPath = params.context.app.uri("css/bundle.css");
+export default function PreviewLayout ({ context }, ...children) {
+  const stylesheetPath = context.app.uri('css/bundle.css')
 
-    return <html>
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" type="text/css" href="/css/normalize.css" />
-          <link media="all" rel="stylesheet" href={stylesheetPath} />
-          <title>Preview Layout</title>
-        </head>
-        <body class="application">
-          {children}
-        </body>
-        <script src="/js/bundle.js"></script>
-    </html>;
+  return <html>
+    <head>
+      <meta charset='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='stylesheet' type='text/css' href='/css/normalize.css' />
+      <link media='all' rel='stylesheet' href={stylesheetPath} />
+      <title>Preview Layout</title>
+    </head>
+    <body class='application'>
+      {children}
+    </body>
+    <script src='/js/bundle.js' />
+  </html>
 }
