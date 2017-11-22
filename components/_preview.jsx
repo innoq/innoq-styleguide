25f8a54ae8bc3832170a39031512abd095a182b2
Robert Glaser <mail@robert-glaser.de>
2017-11-22 14:13:22 +0100

    Fix paths

--- components/_preview.jsx
+++ components/_preview.jsx
@@ -1,19 +1,17 @@
 import { createElement } from 'complate-stream'
 
 export default function PreviewLayout ({ context }, ...children) {
-  const stylesheetPath = context.app.uri('css/bundle.css')
-
   return <html>
     <head>
       <meta charset='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='stylesheet' type='text/css' href='/css/normalize.css' />
-      <link media='all' rel='stylesheet' href={stylesheetPath} />
+      <link rel='stylesheet' type='text/css' href={context.app.uri('css/normalize.css')} />
+      <link media='all' rel='stylesheet' href={context.app.uri('css/bundle.css')} />
       <title>Preview Layout</title>
     </head>
     <body class='application'>
       {children}
     </body>
-    <script src='/js/bundle.js' />
+    <script src={context.app.uri('js/bundle.js')} />
   </html>
 }
