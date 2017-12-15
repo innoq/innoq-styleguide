38ac75d9d7c2b2fc08eba07ed4e9f36af8404bf6
Robert Glaser <mail@robert-glaser.de>
2017-12-15 10:19:27 +0100

    Fix hardcoded image paths

---
+++ components/02-molecules/teaser/podcast-teaser/config.jsx
@@ -0,0 +1,7 @@
+import { context } from '../../../env'
+
+const urlRight = context.uri('static/robert_podcast.png')
+const urlLeft = context.uri('static/robert_podcast_left.png')
+
+export const cssRight = `background-image: url(${urlRight});`
+export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
