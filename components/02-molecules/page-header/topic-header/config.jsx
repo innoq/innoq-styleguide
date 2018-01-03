1a118ef6d7b3507b5c13fd66f220fcd31f50906f
nerdbabe <mail@nerdbabe.de>
2018-01-03 17:20:13 +0100

    Add event tile teaser and landing page layout

---
+++ components/02-molecules/page-header/topic-header/config.jsx
@@ -0,0 +1,5 @@
+import { context } from '../../../env'
+
+const urlBackground = context.uri('static/visual-header-girl.jpg')
+
+export const cssBackground = `background-image: url(${urlBackground});`
