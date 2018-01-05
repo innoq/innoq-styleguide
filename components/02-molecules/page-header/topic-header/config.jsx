37d8fe0c5668dd7e6870a932103c8f51a9a9be3d
nerdbabe <mail@nerdbabe.de>
2018-01-05 11:33:43 +0100

    Remove gradient for background image

--- components/02-molecules/page-header/topic-header/config.jsx
+++ components/02-molecules/page-header/topic-header/config.jsx
@@ -3,5 +3,5 @@ import { context } from '../../../env'
 const urlBackground = context.uri('static/visual-header-igel.jpg')
 const urlBackgroundSmall = context.uri('static/visual-header-igel-1440.jpg')
 
-export const cssBackground = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlBackground});`
-export const cssBackgroundSmall = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlBackgroundSmall});`
+export const cssBackground = `background-image: url(${urlBackground});`
+export const cssBackgroundSmall = `background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url(${urlBackgroundSmall});`
