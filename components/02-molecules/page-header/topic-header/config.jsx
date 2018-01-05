c30da9a620e4b5d97c56aacc7a3f6b60f24a8b7c
nerdbabe <mail@nerdbabe.de>
2018-01-05 13:30:57 +0100

    Add landing page header

--- components/02-molecules/page-header/topic-header/config.jsx
+++ components/02-molecules/page-header/topic-header/config.jsx
@@ -4,4 +4,4 @@ const urlBackground = context.uri('static/visual-header-igel.jpg')
 const urlBackgroundSmall = context.uri('static/visual-header-igel-1440.jpg')
 
 export const cssBackground = `background-image: url(${urlBackground});`
-export const cssBackgroundSmall = `background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url(${urlBackgroundSmall});`
+export const cssBackgroundSmall = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlBackgroundSmall});`
