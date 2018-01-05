01c7d399492846644a0d2913ca68364af8aeeb16
Robert Glaser <mail@robert-glaser.de>
2018-01-05 16:57:28 +0100

    Use koralle

--- components/02-molecules/page-header/topic-header/config.jsx
+++ components/02-molecules/page-header/topic-header/config.jsx
@@ -1,7 +1,7 @@
 import { context } from '../../../env'
 
-const urlBackground = context.uri('static/visual-header-igel.jpg')
-const urlBackgroundSmall = context.uri('static/visual-header-igel-1440.jpg')
+const urlBackground = context.uri('static/visual-header-koralle.jpg')
+const urlBackgroundSmall = context.uri('static/visual-header-koralle.jpg')
 
 export const cssBackground = `background-image: url(${urlBackground});`
 export const cssBackgroundSmall = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlBackgroundSmall});`
