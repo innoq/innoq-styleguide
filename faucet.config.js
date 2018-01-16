aaa745267aff8f5709d44f5ec51971835a749563
Robert Glaser <mail@robert-glaser.de>
2018-01-16 09:52:40 +0100

    Unify pathnames for manifests

--- faucet.config.js
+++ faucet.config.js
@@ -1,29 +1,29 @@
 const config = {
   static: {
     manifest: {
-      file: 'public/static.json',
-      baseURI: (bundlePath, baseName) => `${process.env.BASE_URI || ''}/static/${baseName}`
+      file: 'public/assets/static.json',
+      baseURI: (bundlePath, baseName) => `${process.env.BASE_URI || ''}/assets/static/${baseName}`
     },
     bundles: [
       {
         source: 'lib/images',
-        target: 'public/static'
+        target: 'public/assets/static'
       }
     ]
   },
   sass: {
     manifest: false,
     assets: [
-      'public/static.json'
+      'public/assets/static.json'
     ],
     bundles: [
       {
         entryPoint: 'lib/styles/index.scss',
-        target: 'public/css/bundle.css'
+        target: 'public/assets/stylesheets/bundle.css'
       },
       {
         entryPoint: 'node_modules/normalize.css/normalize.css',
-        target: 'public/css/normalize.css'
+        target: 'public/assets/stylesheets/normalize.css'
       }
     ]
   },
@@ -32,11 +32,11 @@ const config = {
     bundles: [
       {
         entryPoint: 'lib/scripts/index.js',
-        target: 'public/js/bundle.js'
+        target: 'public/assets/javascripts/bundle.js'
       },
       {
         entryPoint: 'node_modules/document-register-element/build/document-register-element.js',
-        target: 'public/js/document-register-element.js'
+        target: 'public/assets/javascripts/document-register-element.js'
       }
     ]
   }
