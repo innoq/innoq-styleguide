02513fe906ce583f03ebd4d13767323313696523
Steffen <mail@steffenhenschel.com>
2018-01-25 12:48:13 +0100

    multi-toggler update, new collapse-toggler
    - added some multi-toggler functionality (init-state, self-class, …)
    - new collapse-toggler based on custom element multi-toggler

--- components/01-atoms/misc/multi-toggler/README.md
+++ components/01-atoms/misc/multi-toggler/README.md
@@ -3,7 +3,13 @@ Toggles a class on elements.
 Expects arguments:
 
 - `data-target`: Selector for Elements which shall be toggled upon
-- `data-toggle-class`: the className which shall be toggled
+- `data-target-next`: Add next sibling element to targets
+- `data-toggle-class`: the className which shall be toggled on targets
+- `data-toggle-on-init`: Toggle targets on init
+- `data-init-class`: add this class initially to targets
+
+- `data-toggle-self-class`: the className which shall be toggled on the toggler itself
+- `data-init-self-class`: a className this toggler should get on itself on init
 
 Example:
 
--- components/01-atoms/misc/multi-toggler/multi-toggler.js
+++ components/01-atoms/misc/multi-toggler/multi-toggler.js
@@ -1,32 +1,48 @@
 export default class MultiToggler extends HTMLElement {
   connectedCallback () {
     let targetSelector = this.getAttribute('data-target')
+    let hasNextElementSiblingTarget = this.hasAttribute('data-target-next')
     this.toggleClass = this.getAttribute('data-toggle-class')
-    if (targetSelector === '') {
-      this.toggleTargets = undefined
-    } else {
-      this.toggleTargets = Array.from(document.querySelectorAll(targetSelector))
+    let initClass = this.getAttribute('data-init-class')
+    let toggleOnInit = this.hasAttribute('data-toggle-on-init')
+    this.toggleSelfClass = this.getAttribute('data-toggle-self-class')
+    let initSelfClass = this.getAttribute('data-init-self-class')
+
+    this.targtElements = []
+    if (targetSelector) {
+      this.targtElements.push(Array.from(document.querySelectorAll(targetSelector)))
+    }
+    if (hasNextElementSiblingTarget) {
+      this.targtElements.push(this.nextElementSibling)
+    }
+    if (toggleOnInit) {
+      this.toggle()
+    }
+    if (initClass) {
+      this.toggleTargets(initClass)
+    }
+    if (initSelfClass) {
+      this.classList.add(initSelfClass)
     }
     this.onclick = this.toggle.bind(this)
-    this.classList.add('enhanced')
   }
 
   toggle () {
-    if (this.toggleTargets) {
-      this.toggleTargets.forEach(
-        target => target.classList.toggle(this.toggleClass)
+    this.toggleTargets(this.toggleClass)
+  }
+
+  toggleTargets (className) {
+    if (this.targtElements[0]) {
+      this.targtElements.forEach(
+        target => target.classList.toggle(className)
       )
       this.toggleSelf()
     }
   }
 
   toggleSelf () {
-    if (this.toggleTargets) {
-      if (this.getAttribute('toggled') !== null) {
-        this.removeAttribute('toggled')
-      } else {
-        this.setAttribute('toggled', '')
-      }
+    if (this.toggleSelfClass) {
+      this.classList.toggle(this.toggleSelfClass)
     }
   }
 }
