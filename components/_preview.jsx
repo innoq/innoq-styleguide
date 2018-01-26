c5798efcc7311309579d39ad5e42c54fc498652a
Robert Glaser <mail@robert-glaser.de>
2018-01-26 15:00:56 +0100

    Unify asset paths

--- components/_preview.jsx
+++ components/_preview.jsx
@@ -5,8 +5,8 @@ export default function PreviewLayout ({ context }, ...children) {
     <head>
       <meta charset='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='stylesheet' type='text/css' href={context.app.uri('assets/stylesheets/normalize.css')} />
-      <link media='all' rel='stylesheet' href={context.app.uri('assets/stylesheets/bundle.css')} />
+      <link rel='stylesheet' type='text/css' href={context.app.uri('assets/normalize.css')} />
+      <link media='all' rel='stylesheet' href={context.app.uri('assets/bundle.css')} />
       <style>{'\
         body {\
           padding: 1rem;\
@@ -17,7 +17,7 @@ export default function PreviewLayout ({ context }, ...children) {
     <body class='application'>
       {children}
     </body>
-    <script src={context.app.uri('assets/javascripts/document-register-element.js')} />
-    <script src={context.app.uri('assets/javascripts/bundle.js')} />
+    <script src={context.app.uri('assets/document-register-element.js')} />
+    <script src={context.app.uri('assets/bundle.js')} />
   </html>
 }
--- faucet.config.js
+++ faucet.config.js
@@ -2,36 +2,36 @@ const path = require('path')
 
 const config = {
   manifest: {
-    file: './public/assets/static.json',
+    file: './public/assets/manifest.json',
     value: filepath => {
       const filename = path.basename(filepath)
-      return `${process.env.BASE_URI || ''}/assets/static/${filename}`
+      return `${process.env.BASE_URI || ''}/assets/${filename}`
     }
   },
   static: [
     {
       source: './lib/images',
-      target: './public/assets/static'
+      target: './public/assets'
     }
   ],
   sass: [
     {
       source: './lib/styles/index.scss',
-      target: './public/assets/stylesheets/bundle.css'
+      target: './public/assets/bundle.css'
     },
     {
       source: './node_modules/normalize.css/normalize.css',
-      target: './public/assets/stylesheets/normalize.css'
+      target: './public/assets/normalize.css'
     }
   ],
   js: [
     {
       source: './lib/scripts/index.js',
-      target: './public/assets/javascripts/bundle.js'
+      target: './public/assets/bundle.js'
     },
     {
       source: './node_modules/document-register-element/build/document-register-element.js',
-      target: './public/assets/javascripts/document-register-element.js'
+      target: './public/assets/document-register-element.js'
     }
   ]
 }
