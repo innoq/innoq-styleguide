1ffc73fe95b26515546b273b6111d7b23dcc4428
FND <fnd@localhost.localdomain>
2018-01-26 11:56:06 +0100

    Upgrade faucet-pipeline

--- faucet.config.js
+++ faucet.config.js
@@ -1,45 +1,39 @@
+const path = require('path')
+
 const config = {
-  static: {
-    manifest: {
-      file: 'public/assets/static.json',
-      baseURI: (bundlePath, baseName) => `${process.env.BASE_URI || ''}/assets/static/${baseName}`
+  manifest: {
+    file: './public/assets/static.json',
+    value: filepath => {
+      let filename = path.basename(filepath)
+      return `${process.env.BASE_URI || ''}/assets/static/${filename}`
+    }
+  },
+  static: [
+    {
+      source: './lib/images',
+      target: './public/assets/static'
+    }
+  ],
+  sass: [
+    {
+      source: './lib/styles/index.scss',
+      target: './public/assets/stylesheets/bundle.css'
     },
-    bundles: [
-      {
-        source: 'lib/images',
-        target: 'public/assets/static'
-      }
-    ]
-  },
-  sass: {
-    manifest: false,
-    assets: [
-      'public/assets/static.json'
-    ],
-    bundles: [
-      {
-        entryPoint: 'lib/styles/index.scss',
-        target: 'public/assets/stylesheets/bundle.css'
-      },
-      {
-        entryPoint: 'node_modules/normalize.css/normalize.css',
-        target: 'public/assets/stylesheets/normalize.css'
-      }
-    ]
-  },
-  js: {
-    manifest: false,
-    bundles: [
-      {
-        entryPoint: 'lib/scripts/index.js',
-        target: 'public/assets/javascripts/bundle.js'
-      },
-      {
-        entryPoint: 'node_modules/document-register-element/build/document-register-element.js',
-        target: 'public/assets/javascripts/document-register-element.js'
-      }
-    ]
-  }
+    {
+      source: './node_modules/normalize.css/normalize.css',
+      target: './public/assets/stylesheets/normalize.css'
+    }
+  ],
+  js: [
+    {
+      source: './lib/scripts/index.js',
+      target: './public/assets/javascripts/bundle.js'
+    },
+    {
+      source: './node_modules/document-register-element/build/document-register-element.js',
+      target: './public/assets/javascripts/document-register-element.js'
+    }
+  ]
 }
 
 module.exports = config
