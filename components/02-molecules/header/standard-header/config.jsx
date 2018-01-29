b1c4efd485cf6c9fde08e76a94958addc395c186
Robert Glaser <mail@robert-glaser.de>
2018-01-29 11:58:22 +0100

    Fix broken paths

--- components/02-molecules/header/standard-header/config.jsx
+++ components/02-molecules/header/standard-header/config.jsx
@@ -1,5 +1,5 @@
 import { context } from '../../../env'
 
-const urlBackground = context.uri('static/visual-header-medusa.jpg')
+const urlBackground = context.uri('assets/visual-header-medusa.jpg')
 
 export const cssBackground = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url(${urlBackground});`
--- components/02-molecules/teaser/list-teaser/podcast-list-teaser/config.jsx
+++ components/02-molecules/teaser/list-teaser/podcast-list-teaser/config.jsx
@@ -1,7 +1,7 @@
 import { context } from '../../../../env'
 
-const urlRight = context.uri('static/heribert-rechts.png')
-const urlLeft = context.uri('static/heribert-links.png')
+const urlRight = context.uri('assets/heribert-rechts.png')
+const urlLeft = context.uri('assets/heribert-links.png')
 
 export const cssRight = `background-image: url(${urlRight});`
 export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
