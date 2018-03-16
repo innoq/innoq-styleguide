4651a14850f0481650fa5433b9fe19c4f955cbe7
nerdbabe <mail@nerdbabe.de>
2018-03-16 09:36:59 +0100

    Add a podcast page header to the SG

--- components/02-molecules/header/standard-header/config.jsx
+++ components/02-molecules/header/standard-header/config.jsx
@@ -1,5 +1,8 @@
 import { context } from '../../../env'
 
 const urlBackground = context.uri('assets/visual-header-medusa.jpg')
+const urlRight = context.uri('assets/heribert-rechts.png')
+const urlLeft = context.uri('assets/heribert-links.png')
 
 export const cssBackground = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url(${urlBackground});`
+export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
