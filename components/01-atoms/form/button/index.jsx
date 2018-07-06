0f9b0300cd9e32cb4e7321c9224f7993e69e2195
nerdbabe <mail@nerdbabe.de>
2018-07-06 15:07:04 +0200

    Refactor button atom

--- components/01-atoms/form/button/index.jsx
+++ components/01-atoms/form/button/index.jsx
@@ -1,10 +1,10 @@
 import { createElement } from 'complate-stream'
 import classnames from 'classnames'
 
-export default function Button ({ variant, inverted }, ...children) {
-  let buttonClass = classnames(`btn btn--${variant}`, { 'btn--inverted': inverted })
+export default function Button ({ size, inverted, cta, light }, ...children) {
+  let buttonClass = classnames(`btn btn--${size}`, { 'btn--light': light }, { 'btn--inverted': inverted }, { 'btn--cta': cta })
   let label
-  if (variant === 'cta') {
+  if (cta) {
     label = children.join('')
   }
   return <button class={buttonClass} data-label={label}>{children}</button>
