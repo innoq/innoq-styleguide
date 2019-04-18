657b871e7c7fcc72b7ccd5925afa581442dcdf85
nanoparsec <ute.mayer@innoq.com>
2019-04-18 14:24:22 +0200

    Add full with teaser component

---
+++ components/02-molecules/visual-elements/teaser/full-width-teaser/default-full-width-teaser/index.jsx
@@ -0,0 +1,34 @@
+import { createElement } from 'complate-stream'
+import classNames from 'classnames'
+import Button from '../../../../../01-atoms/input-elements/button/index.jsx'
+
+export default function DefaultFullWidthTeaser ({ header, caption, href, linkText, simple, punchIn, withButton, additionalClasses, headerAdditionalClasses, align }, ...children) {
+  let leftAligned = (align === 'left')
+  let classname = classNames('default-full-width-teaser', { 'default-full-width-teaser--simple': simple, 'default-full-width-teaser--left-aligned': leftAligned }, additionalClasses)
+  let headerClass
+  if (punchIn) {
+    headerClass = classNames('punch-in', headerAdditionalClasses)
+  }
+
+  return <div class={classname}>
+    <div class='default-full-width-teaser__wrapper'>
+      <div class='default-full-width-teaser__body'>
+        <div>
+          <div class='default-full-width-teaser__caption'>{caption}</div>
+          <div>
+            <div class='default-full-width-teaser__header'>
+              <h1 class={headerClass}>{header}</h1>
+            </div>
+            <div class='default-full-width-teaser__text'>
+              {children}
+            </div>
+            <div class='default-full-width-teaser__footer'>
+              { withButton ? <Button href={href} size='small' cta>{linkText}</Button>
+                : <a href={href} class='link-teaser'>{linkText}</a> }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+}
