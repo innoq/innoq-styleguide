e8e2fefa911d0048bb87eef207d8c7ff0193cc87
nanoparsec <ute.mayer-dohmen@innoq.com>
2021-01-28 16:32:23 +0100

    wip

--- components/02-molecules/visual-elements/header/standard-header/config.jsx
+++ components/02-molecules/visual-elements/header/standard-header/config.jsx
@@ -1,8 +1,8 @@
 import { context } from '../../../../env'
 
-const urlBackground = context.uri('assets/visual-header-medusa.jpg')
-const urlRight = context.uri('assets/heribert-rechts.png')
-const urlLeft = context.uri('assets/heribert-links.png')
+const urlBackground = context.uri('assets/bg-images/edition-02/edition02-02.jpg')
+const urlRight = context.uri('assets/example-content/heribert-rechts.png')
+const urlLeft = context.uri('assets/example-content/heribert-links.png')
 
 export const cssBackground = `background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url(${urlBackground});`
 export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
--- components/02-molecules/visual-elements/teaser/list-teaser/podcast-list-teaser/config.jsx
+++ components/02-molecules/visual-elements/teaser/list-teaser/podcast-list-teaser/config.jsx
@@ -1,7 +1,7 @@
 import { context } from '../../../../../env'
 
-const urlRight = context.uri('assets/heribert-rechts.png')
-const urlLeft = context.uri('assets/heribert-links.png')
+const urlRight = context.uri('assets/example-content/heribert-rechts.png')
+const urlLeft = context.uri('assets/example-content/heribert-links.png')
 
 export const cssRight = `background-image: url(${urlRight});`
 export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
