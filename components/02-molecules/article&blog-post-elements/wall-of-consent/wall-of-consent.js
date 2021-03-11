a3ed2636cf32ddb47e206a5250e2307daa2f46f1
Robert Glaser <robert.glaser@innoq.com>
2021-03-11 11:56:54 +0100

    Make wall-of-consent listen to global events

--- components/02-molecules/article&blog-post-elements/wall-of-consent/wall-of-consent.js
+++ components/02-molecules/article&blog-post-elements/wall-of-consent/wall-of-consent.js
@@ -4,21 +4,13 @@ export default class WallOfConsent extends HTMLElement {
 
     if (localStorage.getItem('consent-to-embeds') === '1') {
       this.revealContent()
-      this.hideInitialElements()
       this.checkbox.setAttribute('checked', '')
     }
 
-    this.checkbox.addEventListener('change', (e) => {
-      if (e.target.checked) {
-        localStorage.setItem('consent-to-embeds', '1')
-        this.revealContent()
-        this.hideInitialElements()
-      } else {
-        localStorage.removeItem('consent-to-embeds')
-        this.removeContent()
-        this.unhideInitialElements()
-      }
-    })
+    this.checkbox.addEventListener('change', this.toggle.bind(this))
+
+    document.body.addEventListener('consent-to-embeds', this.revealContent.bind(this))
+    document.body.addEventListener('disconsent-to-embeds', this.removeContent.bind(this))
   }
 
   get checkbox () {
@@ -29,12 +21,30 @@ export default class WallOfConsent extends HTMLElement {
     const template = this.querySelector('template')
     const templateContent = template.content
 
-    this.content.append(templateContent.cloneNode(true))
+    this.content.replaceChildren(templateContent.cloneNode(true))
     this.prepend(this.content)
+    this.hideInitialElements()
+    this.checkbox.checked = true
   }
 
   removeContent () {
     this.content.innerHTML = ''
+    this.unhideInitialElements()
+    this.checkbox.checked = false
+  }
+
+  toggle (event) {
+    if (event.target.checked) {
+      localStorage.setItem('consent-to-embeds', '1')
+      const event = new Event('consent-to-embeds')
+      document.body.dispatchEvent(event)
+      this.revealContent()
+    } else {
+      localStorage.removeItem('consent-to-embeds')
+      const event = new Event('disconsent-to-embeds')
+      document.body.dispatchEvent(event)
+      this.removeContent()
+    }
   }
 
   hideInitialElements () {
