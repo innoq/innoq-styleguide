6e1af26f60642d3804167fa75d1b7459b121c51c
nanoparsec <ute.mayer-dohmen@innoq.com>
2021-04-09 11:19:24 +0200

    Add clickable-area component

---
+++ components/05-atoms/clickable-area/clickable-area.js
@@ -0,0 +1,25 @@
+// following advice from https://inclusive-components.design/cards
+export default class ClickableArea extends HTMLElement {
+  connectedCallback () {
+    if (this.link) {
+      this.addEventListener('mousedown', this.handleMousedown.bind(this))
+      this.addEventListener('mouseup', this.handleMouseup.bind(this))
+      this.style.cursor = 'pointer'
+    }
+  }
+
+  handleMousedown ({ timeStamp }) {
+    this.down = timeStamp
+  }
+
+  handleMouseup ({ timeStamp }) {
+    if (timeStamp - this.down < 200) {
+      this.link.click()
+    }
+  }
+
+  get link () {
+    return this.querySelector('a')
+  }
+}
+
