11bfff645ea623445469fc78769da30f89568178
nanoparsec <1646402+nanoparsec@users.noreply.github.com>
2021-04-26 07:57:36 +0000

    Deploying to gh-pages from  @ cb93e5d758171069c7ec8e37a466febc080ab2aa 🚀

--- components/raw/standard-header/config.jsx
+++
@@ -1,5 +0,0 @@
-import { context } from '../../../env'
-
-const urlBackground = context.uri('assets/bg-images/general/visual-trainings-01.jpg')
-
-export const cssBackground = `background-image: url(${urlBackground}); background-color: var(--overlay-standard-color);`
