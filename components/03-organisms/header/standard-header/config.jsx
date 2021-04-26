90ac5084189feff99513a178254ea3c98bc37ff5
nanoparsec <ute.mayer-dohmen@innoq.com>
2021-04-26 17:28:42 +0200

    Add podcast cover

--- components/03-organisms/header/standard-header/config.jsx
+++ components/03-organisms/header/standard-header/config.jsx
@@ -1,5 +1,8 @@
 import { context } from '../../../env'
 
 const urlBackground = context.uri('assets/bg-images/general/visual-trainings-01.jpg')
+const urlRight = context.uri('assets/example-content/heribert-rechts.png')
+const urlLeft = context.uri('assets/example-content/heribert-links.png')
 
 export const cssBackground = `background-image: url(${urlBackground}); background-color: var(--overlay-standard-color);`
+export const cssBoth = `background-image: url(${urlRight}), url(${urlLeft});`
