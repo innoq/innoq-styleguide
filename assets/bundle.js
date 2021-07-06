0bad11ca3a70435a9a0556f3851bbe9a17653bac
nanoparsec <1646402+nanoparsec@users.noreply.github.com>
2021-07-06 15:51:56 +0000

    Deploying to gh-pages from  @ cb1dd5ca27cc32e093f34eada4b2ce44026961a2 🚀

--- assets/bundle.js
+++ assets/bundle.js
@@ -69,42 +69,6 @@ class MultiToggler extends HTMLElement {
   }
 }
 
-class InfoBox extends HTMLElement {
-  connectedCallback () {
-    this.classList.add('enhanced');
-    this.setAttribute('open', 'false');
-    this.contentHeight = `${this.content.clientHeight}px`;
-    this.content.style.height = 0;
-    this.teaser.onclick = this.toggle.bind(this);
-  }
-
-  get content () {
-    return this.querySelector('.infobox__content')
-  }
-
-  get teaser () {
-    return this.querySelector('.infobox__teaser')
-  }
-
-  toggle () {
-    this.open = !this.open;
-
-    if (this.open) {
-      this.content.style.height = this.contentHeight;
-    } else {
-      this.content.style.height = 0;
-    }
-  }
-
-  get open () {
-    return this.getAttribute('open') === 'true'
-  }
-
-  set open (value) {
-    this.setAttribute('open', value.toString());
-  }
-}
-
 const EVENT_NAME = 'embeds-consent';
 const STORAGE = 'consent-to-embeds';
 
@@ -318,7 +282,6 @@ class ClickableArea extends HTMLElement {
   }
 }
 
-customElements.define('info-box', InfoBox);
 customElements.define('check-to-toggle', CheckToToggle);
 customElements.define('multi-toggler', MultiToggler);
 customElements.define('nav-bar', Navbar);
