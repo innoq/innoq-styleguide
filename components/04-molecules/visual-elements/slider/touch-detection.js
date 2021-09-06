7cc96cfad3acc18ce6b838e4f5a6a4b62e60e707
LaraP <lara.pourabdolrahim@innoq.com>
2021-09-06 16:01:52 +0200

    Fixed Javascript and SCSS and linked to different files. SCSS not linter conform

--- components/04-molecules/visual-elements/slider/touch-detection.js
+++ components/04-molecules/visual-elements/slider/touch-detection.js
@@ -1,7 +1,7 @@
-export default class Slider extends HTMLElement {
+export default class TouchDetection extends HTMLElement {
   connectedCallback () {
     window.addEventListener('touchstart', function touched () {
-      document.body.classList.add('instructions--touch--active')
+      this.classList.add('instructions--touch--active')
       window.removeEventListener('touchstart', touched, false)
     }, false)
   }
