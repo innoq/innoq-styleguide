9d6f40261d6896429bbbc7366bdd9dc629f74590
LaraP <lara.pourabdolrahim@innoq.com>
2021-09-08 17:14:08 +0200

    Fixed JavaScript and CSS and disabled Linter for unavoidable line

--- components/04-molecules/visual-elements/slider/touch-detection.js
+++ components/04-molecules/visual-elements/slider/touch-detection.js
@@ -1,7 +1,7 @@
 export default class TouchDetection extends HTMLElement {
   connectedCallback () {
     window.addEventListener('touchstart', function touched () {
-      this.classList.add('instructions--touch--active')
+      document.body.classList.add('instructions--touch--active')
       window.removeEventListener('touchstart', touched, false)
     }, false)
   }
