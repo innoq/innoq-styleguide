3b61a6edc926e5132403482abb1af25169d64601
nanoparsec <1646402+nanoparsec@users.noreply.github.com>
2023-01-17 15:20:52 +0000

    Deploying to gh-pages from @ innoq/innoq-styleguide@91c62310c5136b5495c490402c6135630430b0cd 🚀

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,6 +1,3 @@
-(function () {
-'use strict';
-
 class CheckToToggle extends HTMLElement {
   connectedCallback() {
     this.checkbox.onclick = this.toggle.bind(this);
@@ -260,6 +257,79 @@ class Navbar extends HTMLElement {
   }
 }
 
+class Submenu extends HTMLElement {
+  connectedCallback() {
+    let summary = this.querySelector('summary');
+    let ul = this.querySelector('ul');
+    if (!summary && !ul) {
+      return
+    }
+
+    // With out method of animating the collapsing/expanding of the menu, it is
+    // necessary to wrap the content of the menu in a `<div>`
+    this.innerHTML = `<button type="button" aria-expanded="false">${summary.innerHTML}</button><div>${ul.outerHTML}</div>`;
+    this.button.addEventListener('click', () => this.toggle());
+
+    this.navigation.addEventListener('submenu-toggle', (ev) => {
+      if (ev.detail && ev.detail.expanded && ev.target !== this.button) {
+        if (this.button.getAttribute('aria-expanded') === 'true') {
+          this.toggle(false);
+        }
+      }
+    });
+    this.toggle(false);
+  }
+
+  toggle(expanded = !(this.button.getAttribute('aria-expanded') === 'true')) {
+    let button = this.button;
+    button.setAttribute('aria-expanded', expanded);
+    button.dispatchEvent(new CustomEvent('submenu-toggle', { detail: { expanded }, bubbles: true }));
+  }
+
+  get navigation() {
+    return this.closest('ul')
+  }
+
+  get button() {
+    return this.querySelector('button')
+  }
+
+  get div() {
+    return this.querySelector('div')
+  }
+
+  get ul() {
+    return this.querySelector('ul')
+  }
+}
+
+class MenuToggle extends HTMLElement {
+  connectedCallback() {
+    if (!this.button) {
+      return
+    }
+
+    this.hidden = false;
+    this.button.type = 'button';
+    this.button.addEventListener('click', () => this.toggle());
+    this.toggle(false);
+  }
+
+  toggle(expanded = !(this.button.getAttribute('aria-expanded') === 'true')) {
+    let button = this.button;
+    button.setAttribute('aria-expanded', expanded);
+    this.navbar.setAttribute('data-expanded', expanded);
+  }
+
+  get button() {
+    return this.querySelector('button')
+  }
+
+  get navbar() {
+    return this.closest('nav')
+  }
+}
+
 // following advice from https://inclusive-components.design/cards
 class ClickableArea extends HTMLElement {
   connectedCallback() {
@@ -301,8 +371,8 @@ class TouchDetection extends HTMLElement {
 customElements.define('check-to-toggle', CheckToToggle);
 customElements.define('multi-toggler', MultiToggler);
 customElements.define('nav-bar', Navbar);
+customElements.define('sub-menu', Submenu);
+customElements.define('menu-toggle', MenuToggle);
 customElements.define('wall-of-consent', WallOfConsent);
 customElements.define('clickable-area', ClickableArea);
 customElements.define('touch-detection', TouchDetection);
-
-})();
