9cc89f09faae7a830f63872934c24ae7b4c25ca3
Ute Mayer-Dohmen <ute.mayer-dohmen@innoq.com>
2023-03-03 20:51:45 +0100

    Add filter component

--- components/04-molecules/filter/filter.js
+++ components/04-molecules/filter/filter.js
@@ -1,13 +1,7 @@
-const filter = document.querySelector('#filter');
-const industries = document.querySelectorAll('.industry');
-
-filter.addEventListener('change', function() {
-  const selectedCategory = this.value;
-  industries.forEach(function(industry) {
-    if (selectedCategory === 'all' || industry.dataset.category === selectedCategory) {
-      industry.style.display = 'block';
-    } else {
-      industry.style.display = 'none';
-    }
-  });
-});
+export default class AutoSubmitForm extends HTMLElement {
+  connectedCallback() {
+    let form = this.querySelector('form')
+    form.addEventListener('change', form.submit)
+    this.classList.add('enhanced')
+  }
+}
