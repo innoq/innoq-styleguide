0c10efd0a4e53d99c88807a937bb8712d0a6c28d
Thomas Pietschmann <thomas.pietschmann@innoq.com>
2026-02-04 16:21:42 +0100

    refactor: replace clickable-area JavaScript with CSS-only solution
    The clickable-area functionality is now implemented purely with CSS using
    the ::after pseudo-element to extend the first link's click area. This
    provides the same functionality without requiring JavaScript.
    
    Changes:
    - Add _clickable-area.scss with CSS-only implementation
    - Deprecate clickable-area.js (kept for backwards compatibility)
    - Remove custom element registration from lib/scripts/index.js

--- components/05-atoms/clickable-area/clickable-area.js
+++ components/05-atoms/clickable-area/clickable-area.js
@@ -1,3 +1,10 @@
+// DEPRECATED: This JavaScript implementation is no longer needed.
+// The clickable-area functionality is now implemented purely with CSS.
+// See _clickable-area.scss for the CSS-only solution.
+//
+// This file is kept for backwards compatibility but will be removed in a future version.
+// You can safely stop registering this custom element.
+//
 // following advice from https://inclusive-components.design/cards
 export default class ClickableArea extends HTMLElement {
   connectedCallback() {
